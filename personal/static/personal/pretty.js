63a5c4feb1c6343ff0b7a320d8341ff4fc19e4ad
TheOnlyScoops1 <scoopingether@gmail.com>
2024-10-21 12:24:36 -0500

    updating the Pretty.html and style-pretty.css and pretty.js files to make the slideshow gallery funtionality actually work. Curently switching over from pycharm to webstorm for better js, css, and html code detection

--- personal/static/personal/pretty.js
+++ personal/static/personal/pretty.js
@@ -3,29 +3,29 @@ showSlides(slideIndex);
 
 // Next/Previous controls
 function plusSlides(n) {
-    showSlides(slideIndex = n)
+    showSlides(slideIndex += n);
 }
 
 function currentSlides(n) {
-    showSlides(slideIndex += n)
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("Pictures");
-    let dots = document.getElementsByClassName("CirclesUnderPictures");
+    let slides = document.getElementsByClassName("picture");
+    let dots = document.getElementsByClassName("circles");
     if (n > slides.length) {
-        slideIndex += 1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
     for (i = 0; i < slides.length; i++) {
-        slides[1].style.display = "none";
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "")
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += "active";
+    dots[slideIndex-1].className += " active";
 }
