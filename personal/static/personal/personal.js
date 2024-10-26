00f0b5a5dc41f8ede934cf33fa9d600d1fbfd6c5
TheOnlyScoops1 <scoopingether@gmail.com>
2024-10-25 23:15:06 -0500

    Just extending more of the littel details as well as deleteing the unused function in personal.js
    adding more to the personal_acievements page

--- personal/static/personal/personal.js
+++ personal/static/personal/personal.js
@@ -7,15 +7,3 @@ function navBarFunction() {
         x.className = "topnav"
     }
 }
-
-function subButtonOut(sw) {
-    var pic;
-    if (sw == 0) {
-    pic = "{% static 'personal/images/IceCreamPhoto.jpg' %}"
-    } else {
-    pic = "{% static 'personal/images/VanillaIceCream.webp' %}"
-    }
-    document.getElementById('myImage').src = pic;
-    let x = document.getElementsByClassName("iceCreamButton")
-
-}
